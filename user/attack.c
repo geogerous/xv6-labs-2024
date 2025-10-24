d965cd0936e3c529939b89ae5210e353da1e1772
Guo Shudan <13071039796@163.com>
2025-10-24 16:13:07 +0800

    完成了第二个作业

--- user/attack.c
+++ user/attack.c
@@ -8,6 +8,12 @@ main(int argc, char *argv[])
 {
   // your code here.  you should write the secret to fd 2 using write
   // (e.g., write(2, secret, 8)
-
+  char *end = sbrk(PGSIZE *32);
+  end = end + PGSIZE * 16;
+  char secret[8 + 1];
+  secret[8] = 0;
+  strcpy(secret, end + 32);
+  printf("%s\n", secret);
+  write(2, secret, 8);
   exit(1);
 }
